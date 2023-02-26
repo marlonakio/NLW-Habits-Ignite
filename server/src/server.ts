c3131242559f377a1f3be5ecd218e45f49c12368
Marlon Akio Tochiro <marlon.akto@gmail.com>
2023-02-26 20:56:24 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+//importação de libs
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from './routes'
+
+const app = fastify()
+
+app.register(cors)
+app.register(appRoutes)
+
+app.listen({
+    host: '0.0.0.0',
+    port: 3333,
+}).then(() => {
+    console.log('HTTP Server running!')
+})
